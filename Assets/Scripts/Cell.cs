2a4699abbe431b82a7686b170d26e6aba2a56675
JoseMagg <jose.gonzalezg1@udea.edu.co>
2023-07-24 10:44:03 -0500

    Cell Assigner

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour
+{
+    [Header("Cell Properties")]
+
+    public int row;
+    public int col;
+    public string nombre;
+    public InstrumentUI instrumentUI;
+    public Instrument instrument;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => ActivarDesactivar());
+    }
+
+    void Activar()
+    {
+        instrumentUI.ChangeSpriteCell(true, this.row, this.col);
+
+        instrument.Pages[instrument.ActualPage].Grid[row, col].isActive = true;
+        instrument.Pages[instrument.ActualPage].Grid[row, col].name = nombre;
+    }
+    void Desactivar()
+    {
+
+        instrumentUI.ChangeSpriteCell(false, this.row, this.col);
+
+        instrument.Pages[instrument.ActualPage].Grid[row, col].isActive = false;
+        instrument.Pages[instrument.ActualPage].Grid[row, col].name = nombre;
+
+    }
+
+    public void ActivarDesactivar()
+    {
+        if (instrument.Pages[instrument.ActualPage].Grid[row, col].isActive)
+        {
+            Desactivar();
+        }
+        else
+        {
+            Activar();
+        }
+        CompositorUI.instance.newChanges = true;
+    }
+
+
+}
+
