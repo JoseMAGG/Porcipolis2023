ee0aeacefc11d71250c59424336c8b4f789497c7
JoseMagg <jose.gonzalezg1@udea.edu.co>
2023-08-05 10:16:09 -0500

    configuraciones de esceneas para apk

--- Assets/Scripts/Tamagotchi/Extras/AccionesCerdoBtn.cs
+++ Assets/Scripts/Tamagotchi/Extras/AccionesCerdoBtn.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AccionesCerdoBtn : MonoBehaviour
 {
     public GestorTamagotchi gestorTamagotchi;
+    public GameObject panel;
+    public float tiempoPanel;
+
+    public void Start()
+    {
+        panel.SetActive(false);
+    }
 
     public void Jugar()
     {
+        StartCoroutine(ActivarPanel());
         gestorTamagotchi.EjecutarAccion(Accion.accion.jugar);
     }
 
@@ -30,6 +37,7 @@ public class AccionesCerdoBtn : MonoBehaviour
         try
         {
             gestorTamagotchi.EjecutarAccion(Accion.accion.comer);
+            StartCoroutine(ActivarPanel());
             GestionCerdoUI.singleton.ActivarObjetos(comida - 1);
         }
         catch (System.Exception e)
@@ -41,24 +49,36 @@ public class AccionesCerdoBtn : MonoBehaviour
     public void Beber()
     {
         gestorTamagotchi.EjecutarAccion(Accion.accion.beber);
+        StartCoroutine(ActivarPanel());
     }
 
     public void Consentir()
     {
         gestorTamagotchi.EjecutarAccion(Accion.accion.consentir);
+        StartCoroutine(ActivarPanel());
     }
 
     public void Bañar()
     {
         gestorTamagotchi.EjecutarAccion(Accion.accion.bañar);
+        StartCoroutine(ActivarPanel());
     }
 
     public void Embarrar()
     {
         gestorTamagotchi.EjecutarAccion(Accion.accion.enmugrar);
+        StartCoroutine(ActivarPanel());
     }
     public void Sanar()
     {
         gestorTamagotchi.EjecutarAccion(Accion.accion.sanar);
+        StartCoroutine(ActivarPanel());
+    }
+
+    private IEnumerator ActivarPanel()
+    {
+        panel.SetActive(true);
+        yield return new WaitForSeconds(tiempoPanel);
+        panel.SetActive(false);
     }
 }
