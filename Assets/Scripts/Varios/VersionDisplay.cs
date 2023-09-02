782265278671a5285530bf65b916a18e6b5e9362
inquernon <inquernon@gmail.com>
2023-09-02 09:35:44 -0500

    correccion de los limites del canvas

---
+++ Assets/Scripts/Varios/VersionDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionDisplay : MonoBehaviour
+{
+    public Text versionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string appVersion = Application.version; // Obtiene la versión de la aplicación
+        versionText.text = "Versión Actual: 2." + appVersion; // Asigna la versión al componente de texto
+
+    }
+}
